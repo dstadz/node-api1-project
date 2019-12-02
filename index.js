5f1af79e8a11332de114b8fc6d772898ec612cfc
Daniel <danstad2012@gmail.com>
2019-12-02 10:41:44 -0800

    checking hubs

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // implement your API here
+const express = require("express")    
+const db = require('./data/')
+
+const server = express()
+const port = 4000
+
+server.get('/', (req, res) => {
+  res.send( {api: 'running like wind'})
+})
+
+server.get('/hubs', (req,res) => {
+  db.find()
+  .then( hubs => {
+    res.status(200).json(hubs)
+  })
+  .catch(err => {
+    console.log('you done goofed with:', err)
+    res
+    .status(500)
+    .json({errorMessage: 'error getting list of hubs from datbase'})
+  })
+})
+
+server.listen(port, () => 
+console.log(`\n API running on port ${port} \n`)
+);
