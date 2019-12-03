e773f00988c86ac195bfac99a552a13621afaeac
Daniel <danstad2012@gmail.com>
2019-12-02 19:16:21 -0800

    cleaning up work

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ const server = express()
 const port = 5000
 server.use(express.json()) // <<<<<<<<<<<<<<<<<<<<<<< important to parse json from body
 
-server.get('/', (req, res) => {
-  res.send( {api: 'running like wind'})
-})
-
 server.post('/api/users', (req, res) => {
   const user = req.body
-  db.insert(user)
+  if(!(user.name && user.bio)) res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+  else {
+
+    db.insert(user)
     .then( rex => {
-      res.status(200).json(rex)
+      res.status(201).json(rex)
     })
     .catch(err => {
       console.log('you done goofed with:', err)
       res
       .status(500)
-      .json({errorMessage: 'error posting list of user to database'})
+      .json({errorMessage: 'error posting user to database'})
     })
+  }
 })
 
 server.get('/api/users', (req, res) => {
@@ -33,7 +33,7 @@ server.get('/api/users', (req, res) => {
     console.log('you done goofed with:', err)
     res
     .status(500)
-    .json({errorMessage: 'error getting list of users from database'})
+    .json({errorMessage: 'error getting users from database'})
   })
 })
 
@@ -51,6 +51,7 @@ server.get('/api/users/:id', (req, res) => {
   })
 })
 
+//won
 server.delete('/api/users/:id', (req, res) => {
   const id = req.params.id
   db.remove(id)
@@ -67,10 +68,27 @@ server.delete('/api/users/:id', (req, res) => {
 })
 
 server.put('/api/users/:id', (req, res) => {
-  db.update(id,user)
-  .then( user=> {
-    res.status(200).json(user)
+
+  const id = req.params.id
+  const user = req.body
+  //const bio = req.body.bio
+  db.findById(id)
+  .then( users => {
+    console.log(users)
+    if(users) res.status(200).json(users)
+    else res.status(404).json({message:"no user with that id found"})
   })
+  if(!(user.name && user.bio)) res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+  else {
+
+  }
+    db.update(id, user)
+    .then(user => {
+      server.put('/api/users/:id',(req,res) => {
+        console.log()
+      })
+    })
+  }
   .catch(err => {
     console.log('you done goofed with:', err)
     res
